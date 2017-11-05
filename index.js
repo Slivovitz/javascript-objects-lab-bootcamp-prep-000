28a98fc2be9c1051511af49d16ddad5a75638af0
Yosef Margolin <yukele@hotmail.com>
2017-11-05 02:27:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
   return recipes
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return recipes
+}
