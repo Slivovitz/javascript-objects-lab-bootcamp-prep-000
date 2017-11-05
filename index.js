7467c761e23d52112347c53a333e67df12cf0c2a
Yosef Margolin <yukele@hotmail.com>
2017-11-05 03:39:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
-  // return recipes
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
-  // return recipes
 }
 
 function deleteFromObjectByKey(object, key) {
@@ -19,5 +17,5 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
-  // return recipes
+  return object
 }
